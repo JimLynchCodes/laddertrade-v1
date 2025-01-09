884c188a9d7178f070859fb25783fbf02197683b
JimLynchCodes <mrdotjim@gmail.com>
2025-01-09 12:29:16 -0500

    update gitignore

--- laddertrader/src/app/schwab-tokens.service.ts
+++ laddertrader/src/app/schwab-tokens.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SchwabTokensService {
+  
+  stuff() {
+    console.log(environment.API_KEY)
+  }
 
   constructor() { }
 }
---
+++ laddertrader/src/environments/environment.ts
@@ -0,0 +1,7 @@
+// This file is just an example. 
+// Create a file named "environment.development.ts" with the same variables names and real values.
+
+export const environment = {
+
+    API_KEY: "foobarrr"
+};
