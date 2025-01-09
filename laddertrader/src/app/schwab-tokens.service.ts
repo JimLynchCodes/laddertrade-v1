3cda89b217ff833a7529b6ae322006c5a7631ed4
JimLynchCodes <mrdotjim@gmail.com>
2025-01-09 13:39:32 -0500

    doesn't seem to work...

--- laddertrader/src/app/schwab-tokens.service.ts
+++ laddertrader/src/app/schwab-tokens.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SchwabTokensService {
   
-  stuff() {
-    console.log(environment.APP_KEY)
-    console.log(environment.SECRET)
+  constructor(private http: HttpClient, private router: Router) { 
+
+    // https://api.schwabapi.com/v1/oauth/authorize?client_id=_&redirect_uri=https://0.0.0.0
+    // https://api.schwabapi.com/v1/oauth/authorize?client_id=_&redirect_uri=https://0.0.0.0
   }
 
-  constructor() { }
+  stuff() {
+
+    const auth_url = `https://api.schwabapi.com/v1/oauth/authorize?client_id=${environment.APP_KEY}&redirect_uri=https://www.localhost:4200`
+    // this.router.navigate([auth_url])
+
+    window.location.href = auth_url;
+
+    // console.log(environment.APP_KEY)
+    // console.log(environment.SECRET)
+
+    // this.http.get(auth_url).subscribe((val) => {
+    //   console.log(val)
+    // })
+
+  }
+
+
+
+
+  constructInitauthUrl() {
+
+  }
 }
